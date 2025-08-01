6edbc6866c31b7d067402870e57a045489c55481
victorMwatu <victormwatu@gmail.com>
2025-08-01 22:15:09 +0300

    complete react tdd testing user interfaces lab

--- src/App.js
+++ src/App.js
@@ -1,5 +1,13 @@
 function App() {
-  return <div>Your code here</div>;
+  return (
+    <div>
+      <h1>Hi, I'm (your name)</h1>
+      <img src="url.url" alt="this image" />
+      <h2>About Me</h2>
+      <p>Yada yada yada</p>
+      <a href="www.github.github">github</a>
+      <a href="www.linkedin.github">linkedin</a>
+    </div>
+  );
 }
-
 export default App;
--- src/__tests__/App.test.js
+++ src/__tests__/App.test.js
@@ -3,4 +3,52 @@ import '@testing-library/jest-dom';
 
 import App from "../App";
 
-// Your tests here
+test("displays a top-level heading with the text `Hi, I'm _______`", () => {
+  // Arrange
+  render(<App />);
+
+  // Act
+  const topLevelHeading = screen.getByRole("heading", {
+    name: /hi, i'm/i,
+    exact: false,
+    level: 1,
+  });
+
+  // Assert
+  expect(topLevelHeading).toBeInTheDocument();
+});
+
+test("display image with an alt text describing the image", () => {
+    render(<App />);
+    const image = screen.getByRole("img");
+    expect(image).toHaveAttribute("alt");
+    expect(image).toHaveAttribute("src");
+});
+
+test("display a second level heading about me", () => {
+    render(<App />);
+    const secondHeading = screen.getByRole("heading", {
+        level: 2,
+        name: /About me/i
+    });
+    expect(secondHeading).toBeInTheDocument();
+});
+
+test("display a paragraph for biography", () => {
+    const {container} = render(<App />);
+    const paragraph = container.querySelector("p");
+    expect(paragraph).toBeInTheDocument();
+});
+
+test("has GitHub and LinkedIn links", () => {
+  render(<App />);
+
+  const gitHubLink = screen.getByRole("link", { name: /github/i });
+  const linkedInLink = screen.getByRole("link", { name: /linkedin/i });
+
+  expect(gitHubLink).toBeInTheDocument();
+  expect(linkedInLink).toBeInTheDocument();
+
+  expect(gitHubLink).toHaveAttribute("href", expect.stringContaining("github"));
+  expect(linkedInLink).toHaveAttribute("href", expect.stringContaining("linkedin"));
+});
